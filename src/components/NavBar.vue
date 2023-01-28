41dd67f23d24b3a3ffda2aaeea312983922c6b39
RocioRDev <rociorgdev@gmail.com>
2023-01-29 00:49:07 +0100

    Trabajando en el Navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -12,7 +12,7 @@
     >
       <el-menu-item>
         <div class="Logo">
-          <el-icon :size="30"> <Goods /> </el-icon> FAKE SHOP
+          <el-icon :size="30"> <Goods /> </el-icon> {{ title }}
         </div>
       </el-menu-item>
       <div class="flex-grow" />
@@ -33,3 +33,99 @@
     </el-menu>
   </nav>
 </template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+let title = "FAKE SHOP";
+
+@Options({
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default: title,
+    },
+  },
+})
+export default class NavBar extends Vue {
+  title!: string;
+}
+</script>
+
+<style scoped>
+nav {
+  width: 100% !important;
+  min-height: 50px;
+  background-color: coral;
+}
+
+nav ul li a {
+  text-decoration: none;
+  width: 100%;
+  text-align: center;
+  color: "#fff" !important;
+}
+
+nav ul li {
+  min-width: 100px !important;
+  padding: 0 !important;
+  text-decoration: none !important;
+  list-style: none;
+  color: "#fff" !important;
+}
+
+ul {
+  display: flex;
+  justify-content: center;
+  list-style: none;
+}
+
+li {
+  font-weight: bold;
+}
+
+a {
+  color: white;
+  text-decoration: none;
+}
+
+a:visited {
+  color: white;
+}
+
+.logout-button {
+  color: white;
+  text-decoration: none;
+}
+
+.logout-button:visited {
+  color: white;
+}
+
+.flex-grow {
+  flex-grow: 1;
+}
+
+.Logo {
+  margin-left: 60px;
+  display: flex;
+  align-items: center;
+}
+
+.el-menu-demo {
+  border-bottom: 1px solid #e4e4e4;
+}
+
+.el-menu-demo .el-menu-item {
+  padding: 0 20px;
+}
+
+.el-menu-demo .el-menu-item.is-active {
+  color: #ffd04b;
+}
+
+.el-menu-demo .el-menu-item.is-active .el-icon {
+  color: #ffd04b;
+}
+</style>
